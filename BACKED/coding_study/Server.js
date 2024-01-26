80c4128484957b335acd4139fec26b983650ec54
garusitell <155941254+zzim2003@users.noreply.github.com>
2024-01-26 12:33:22 +0900

    1

--- BACKED/coding_study/Server.js
+++ BACKED/coding_study/Server.js
@@ -135,3 +135,11 @@ app.post("/edit",function(req,res){
 app.get("/",function(req, res){
   res.render("index.ejs");
 });
+
+let cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.get('/cookie',function(req,res){
+  res.cookie('milk','1000원');
+  res.send('product: '+req.cookies.milk);
+});
